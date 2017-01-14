acd4b1daa259d87bf1ba97ee54b8a6e73a760671
samthomson <samt@samt.st>
2017-01-14 19:58:39 +0100

    get todos route

--- server/server.js
+++ server/server.js
@@ -11,6 +11,16 @@ var app = express();
 
 app.use(bodyParser.json());
 
+app.get('/todos', (req, res) => {
+    Todo.find().then(
+        (todos) => {
+            res.send({todos});
+        }, (e) => {
+            res.status(400).send(e);
+        }
+    );
+});
+
 app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
