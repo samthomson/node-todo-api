bea3bb9cecc7124530666e497dbd79a7442692d6
samthomson <samt@samt.st>
2017-01-15 17:52:49 +0100

    testing

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -2,22 +2,24 @@
 const expect = require('expect');
 const request = require('supertest');
 
+const {ObjectID} = require('mongodb');
+
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+    _id: new ObjectID(),
     text: 'first test todo'
 },
 {
+    _id: new ObjectID(),
     text: 'second test todo'
 }];
 
 beforeEach((done) => {
-    Todo.remove({})
-        .then(() => {
-            return Todo.insertMany(todos);
-        })
-        .then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -59,7 +61,7 @@ describe('POST /todos', () => {
                     done();
                 }).catch((e) => done(e));
             });
-    })
+    });
 });
 
 describe('GET /todos', () => {
@@ -72,4 +74,25 @@ describe('GET /todos', () => {
             })
             .end(done);
     });
+});
+
+describe('GET /todos/:id', () => {
+    it('should return todo doc', (done) => {
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(todos[0].text);
+            })
+            .end(done);
+    });
+    /*
+    it('should return 404 if todo not found', (done) => {
+
+    });
+
+    it('should return 404 for non-object ids', (done) => {
+        // /todos/sam
+    });
+    */
 });
