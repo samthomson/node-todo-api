c39998fe3adae0563a7b655d7df69e9d44e579dc
samthomson <samt@samt.st>
2017-01-21 22:09:02 +0100

    pass hashing

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123pass';
 
-var token = jwt.sign(data, 'chilli-flakes');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
 
+var hashedPassword = '$2a$10$ekOGSG9zCnZFtv8WGgOOEOaSN0GYCNAJPQyWFRtLKv.CKR.CmS6eC';
 
-var decoded = jwt.verify(token, 'chilli-flakes');
-console.log("decoded ", decoded);
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
+
+// var data = {
+//     id: 10
+// };
+//
+// var token = jwt.sign(data, 'chilli-flakes');
+// console.log(token);
+//
+//
+// var decoded = jwt.verify(token, 'chilli-flakes');
+// console.log("decoded ", decoded);
 
 
 // var message = 'I am a user';
