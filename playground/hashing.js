a060a64a5cd58cba706b617f27a55a5e93f131a9
samthomson <samt@samt.st>
2017-01-21 20:52:28 +0100

    jwt

---
+++ playground/hashing.js
@@ -0,0 +1,42 @@
+const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+var data = {
+    id: 10
+};
+
+var token = jwt.sign(data, 'chilli-flakes');
+console.log(token);
+
+
+var decoded = jwt.verify(token, 'chilli-flakes');
+console.log("decoded ", decoded);
+
+
+// var message = 'I am a user';
+// var hash = SHA256(message).toString();
+//
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`);
+//
+// var data = {
+//     id: 4
+// };
+// var token = {
+//     data,
+//     hash: SHA256(JSON.stringify(data) + 'secret-salt').toString()
+// }
+//
+//
+// token.data.id = 5;
+// token.hash = SHA256(JSON.stringify(token.data)).toString();
+//
+//
+// var resultHash = SHA256(JSON.stringify(token.data) + 'secret-salt').toString();
+//
+// if(resultHash === token.hash) {
+//     // not manipulated
+//     console.log('data not changed');
+// } else {
+//     console.log('Data was changed. Don\'t trust');
+// }
